5f2b5bc99b37d76eb079b77c345299d910a49fca
Christoph Lütjen <info@christoph-luetjen.de>
2019-03-19 16:56:58 +0100

    some more crlf > lf

--- support/docker/docker-build.sh
+++ support/docker/docker-build.sh
@@ -7,10 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../..
 
-# PREPARE
-# Workaround: environment.ts does not exist in repo but angular cli needs it to be able to overwrite it with environment.prod.ts
-touch src/environments/environment.ts
-
 # BUILD
 hugo
 if [ $? -ne 0 ]; then
